d65cc0f56100d0f05e07522ab1c8e72cc54e7816
Bob van den Heuvel <bvandenheuvel@schubergphilis.com>
2015-09-28 16:55:03 +0200

    wip

---
+++ CS_KEYS.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+export CLOUDSTACK_API_KEY=
+export CLOUDSTACK_SECRET_KEY=
+export CLOUDSTACK_HOST=
+export PUBLIC_SOURCE_NAT_IP=
+export NETWORK_NAME=
+export SERVICE_OFFERING_NAME=
+export ZONE_NAME=
+
+
+
+
+
+
+
+
